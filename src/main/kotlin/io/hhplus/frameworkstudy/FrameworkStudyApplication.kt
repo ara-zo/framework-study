7493b2518abcf72aac160016c01aa79c120e4bca
ara-zo <jo91kr@gmail.com>
2024-11-14 22:12:54 +0900

    :tada: Initial commit

---
+++ src/main/kotlin/io/hhplus/frameworkstudy/FrameworkStudyApplication.kt
@@ -0,0 +1,11 @@
+package io.hhplus.frameworkstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrameworkStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrameworkStudyApplication>(*args)
+}
